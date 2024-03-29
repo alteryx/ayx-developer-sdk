74ef561e0eaa5b04ca2c6ed3506eb172db902df4
derek.andreis <derek.andreis@alteryx.com>
2024-03-29 22:00:17 +0000

    corrected statement about input tools

--- docs/references/amp-constants-example.py
+++ docs/references/amp-constants-example.py
@@ -40,7 +40,7 @@ class ConstantsExample(PluginV2):
         anchor
             NamedTuple containing anchor.name and anchor.connection.
         """
-        raise NotImplementedError("Input tools don't receive batches.")
+        raise NotImplementedError("Input tools don't receive incoming connections.")
 
     def on_record_batch(self, batch: "Table", anchor: namedtuple) -> None:
         """
